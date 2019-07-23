c9b8c419b21f38f174fe9419ce07cdd8136c5128
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-07-22 21:13:33 -0400

    removing li item

--- src/reducers/todoitems.js
+++ src/reducers/todoitems.js
@@ -1,6 +1,16 @@
 //import action
 
-export default function todoItems(state = [], action) {
-    console.log(state, action)
+export default function postTodoItems(state = [], action) {
+    switch (action.type) {
+        case 'REMOVE_TO_DO_ITEM' : 
+            console.log('Removing todo Items!');
+            return [
+                ...state.slice(0, action.index),
+                ...state.slice(action.index + 1, state.length)
+            ]
+        default: 
+            return state
+    }
+    
     return state
 }
